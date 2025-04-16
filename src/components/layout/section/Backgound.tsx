01e76627769197b1da7bdeb87db7ae8723e04ba8
nurkyzzz006 <nasnur399@gmail.com>
2025-04-16 12:17:10 +0600

    the close

---
+++ src/components/layout/section/Backgound.tsx
@@ -0,0 +1,13 @@
+import scss from './Background.module.scss'; 
+const Backgound = () => {
+     return (
+      <div id={scss.Backgound}>
+        <div className='container'>
+         <div className={scss.content}>
+            
+         </div>
+        </div>
+      </div>
+    );
+};
+export default Backgound;
